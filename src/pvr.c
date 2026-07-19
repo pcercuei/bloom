pcercuei/bloom

commit f77688ae5e8e55442686bbfb3c3bce5bbfa71c28

pvr: Fix non-black transparent pixels in mask texture
The mask texture had the original texture's semi-transparent pixels as
transparent, but not as fully-black transparent. This was causing some
blending operations to render incorrectly.

Signed-off-by: Paul Cercueil <paul@crapouillou.net>

src/pvr.c
@@ -573,13 +573,16 @@ static void load_palette(struct texture_page *page, unsigned int offset,
 		 * semi-transparent or not. */
 		if (pixel != 0x0000) {
 			color = bgr_to_rgb(pixel);
+
+			if (likely(!(clut & CLUT_IS_MASK)))
+				color |= 0x8000;
+			else if (color & 0x8000)
+				color = 0;
+
 			color |= color << 16;
 			color |= color << 32;
 
-			if (clut & CLUT_IS_MASK)
-				sq[i] = color ^ 0x8000800080008000ull;
-			else
-				sq[i] = color | 0x8000800080008000ull;
+			sq[i] = color;
 		} else {
 			sq[i] = 0;
 		}

commit 7725c57f123c3bc07ab212b391e3d450b290f621

pvr: Fix mask texture once again
Commit f77688ae5 ("pvr: Fix non-black transparent pixels in mask
texture") broke things, as non-semi-transparent pixels were no longer
set as opaque pixels.

Signed-off-by: Paul Cercueil <paul@crapouillou.net>

src/pvr.c
@@ -573,9 +573,9 @@ static void load_palette(struct texture_page *page, unsigned int offset,
 		if (pixel != 0x0000) {
 			color = bgr_to_rgb(pixel);
 
-			if (likely(!(clut & CLUT_IS_MASK)))
+			if (likely(!(clut & CLUT_IS_MASK)) || !(color & 0x8000))
 				color |= 0x8000;
-			else if (color & 0x8000)
+			else
 				color = 0;
 
 			color |= color << 16;